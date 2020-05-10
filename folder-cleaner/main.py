2d342a29834599579f07c7b3657438438640dfe8
Leovaldez42 <gaurav4.037@gmail.com>
2020-05-11 00:02:04 +0530

    basic-projects

---
+++ folder-cleaner/main.py
@@ -0,0 +1,39 @@
+import os       # can be installed by pip install os
+
+def createIfNotExist(folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+        
+def move(folder_name, files):
+    for file in files:
+        os.replace(file, f"{folder_name}/{file}")
+
+if __name__ == "__main__":
+    
+    files = os.listdir()
+    files.remove("main.py")
+
+    createIfNotExist('Images')
+    createIfNotExist('Docs')
+    createIfNotExist('Media')
+    createIfNotExist("Others")
+
+    imgExt = [".png", '.jpg', '.jpeg']
+    images = [file for file in files if os.path.splitext(file)[1].lower in imgExt]
+
+    docExts = [".txt", ".docs", ".doc", ".pdf"]
+    docs = [file for file in files if os.path.splitext(file)[1] in docExts]
+
+    mediaExts = [".mp4", ".mp3", ".flv"]
+    medias = [file for file in files if os.path.splitext(file)[1] in mediaExts]
+
+    others = []
+    for file in files:
+        ext = os.path.splitext(file)[1].lower()
+        if (ext not in mediaExts) and (ext not in docExts) and (ext not in imgExt) and os.path.isFile(file):
+            others.append(file)
+
+    move("Images", images)
+    move("Docs", docs)
+    move("Media", medias)
+    move("Others", others)
---
+++ water-reminder/main.py
@@ -0,0 +1,14 @@
+import time
+from plyer import notification      # can be installed by pip install plyer
+
+if __name__ == "__main__":
+    while True:
+        notification.notify(
+            title = "Please drink Water",
+            message = 'You are really not drinking enough water now a days. Go drink some   without delay.',
+            app_icon = "icon.png",
+            timeout = 10        # Will stay for 10 seconds.
+        )
+        time.sleep(60*60)       # Will remind after one hour.
+        
+        # In case you want to close the program and the notification should keep coming run the program using pythonw ./main.py ( after going to the folder ).
